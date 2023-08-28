1a48392cdac041995fad20699f44c898989e132e
Merk0n <127611734+Merk0n@users.noreply.github.com>
2023-08-28 22:40:25 +0200

    added UI

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+let playerPoints = 0;
+let computerPoints = 0;
+
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
     case 0:
-      console.log("Computer chose rock");
       return "rock";
     case 1:
-      console.log("Computer chose paper");
       return "paper";
     case 2:
-      console.log("Computer chose scissors");
       return "scissors";
   }
 };
@@ -29,49 +29,122 @@ const playGame = (playerSelection, computerSelection) => {
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
       computerPoints++;
-      return "Computer wins!";
+      return "Computer";
     } else {
       playerPoints++;
-      return "Player wins!";
+      return "Player";
     }
   }
   if (playerSelection === "paper") {
     if (computerSelection === "scissors") {
       computerPoints++;
-      return "Computer wins!";
+      return "Computer";
     } else {
       playerPoints++;
-      return "Player wins!";
+      return "Player";
     }
   }
   if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
       computerPoints++;
-      return "Computer wins!";
+      return "Computer";
     } else {
       playerPoints++;
-      return "Player wins!";
+      return "Player";
     }
   }
 };
 
-let playerPoints = 0;
-let computerPoints = 0;
+//queryselectors
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const playerScore = document.querySelector(".player-score");
+const computerScore = document.querySelector(".computer-score");
+const result = document.querySelector(".result");
+const winner = document.querySelector(".winner");
+const userChoice = document.querySelector(".user-choice");
+const computerChoice = document.querySelector(".computer-choice");
+const reset = document.querySelector(".reset");
+const final = document.querySelector(".final");
 
-// loop game 5 times
-while (playerPoints < 5 && computerPoints < 5) {
-  let playerSelection = prompt("Choose rock, paper or scissors");
-  playerSelection = playerSelection.toLowerCase();
-  console.log(playGame(playerSelection, getComputerChoice()));
-}
+//event listeners
+rock.addEventListener("click", function () {
+  const computerSelection = getComputerChoice();
+  const playerSelection = "rock";
+  const gameResult = playGame(playerSelection, computerSelection);
+  winner.textContent = gameResult;
+  userChoice.textContent = playerSelection;
+  computerChoice.textContent = computerSelection;
+  playerScore.textContent = playerPoints;
+  computerScore.textContent = computerPoints;
+  console.log(playerPoints, computerPoints);
+  checkIfGameOver();
+});
 
-// check who won
-if (playerPoints > computerPoints) {
-  console.log("*Player won the game!*");
-} else if (playerPoints < computerPoints) {
-  console.log("*Computer won the game!*");
-} else {
-  console.log("*It's a tie!*");
-}
+paper.addEventListener("click", function () {
+  const computerSelection = getComputerChoice();
+  const playerSelection = "paper";
+  const gameResult = playGame(playerSelection, computerSelection);
+  winner.textContent = gameResult;
+  userChoice.textContent = playerSelection;
+  computerChoice.textContent = computerSelection;
+  playerScore.textContent = playerPoints;
+  computerScore.textContent = computerPoints;
+  console.log(playerPoints, computerPoints);
+  checkIfGameOver();
+});
 
-console.log("xd");
+scissors.addEventListener("click", function () {
+  const computerSelection = getComputerChoice();
+  const playerSelection = "scissors";
+  const gameResult = playGame(playerSelection, computerSelection);
+  winner.textContent = gameResult;
+  userChoice.textContent = playerSelection;
+  computerChoice.textContent = computerSelection;
+  playerScore.textContent = playerPoints;
+  computerScore.textContent = computerPoints;
+  console.log(playerPoints, computerPoints);
+  checkIfGameOver();
+});
+
+reset.addEventListener("click", function () {
+  playerPoints = 0;
+  computerPoints = 0;
+  playerScore.textContent = playerPoints;
+  computerScore.textContent = computerPoints;
+  winner.textContent = "";
+  userChoice.textContent = "";
+  computerChoice.textContent = "";
+  final.textContent = "";
+  enableButtons();
+});
+
+//function that checks if the game is over
+const checkIfGameOver = () => {
+  if (playerPoints === 5 || computerPoints === 5) {
+    final.textContent = "Game over!";
+    if (playerPoints > computerPoints) {
+      final.textContent += " You won!";
+    } else {
+      final.textContent += " Computer won!";
+    }
+    disableButtons();
+  } else {
+    final.textContent = "";
+  }
+};
+
+//function which disable
+const disableButtons = () => {
+  rock.disabled = true;
+  paper.disabled = true;
+  scissors.disabled = true;
+};
+
+//function which enable
+const enableButtons = () => {
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
+};
